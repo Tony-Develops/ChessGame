e406237b09ba047a0d6109d9a890221f20769057
Daniel <s3719271@student.rmit.edu.au>
2018-08-17 10:15:09 +1000

    Added ChessPiece class.

--- src/chessPieces/ChessPiece.java
+++ src/chessPieces/ChessPiece.java
@@ -1,5 +1,26 @@
 package chessPieces;
 
-public class ChessPiece {
-
+public abstract class ChessPiece {
+	
+	private Position pos;
+	private Type type;
+	
+	public void ChessPiece(Position pos, Type type) {
+		this.pos = pos;
+		this.type = type;
+	}
+	
+	public void update(Position destPos) {
+		pos = destPos;
+	}
+	
+	public abstract calcPositions();
+	
+	public Position getPos() {
+		return pos;
+	}
+	
+	public Type getType() {
+		return type;
+	}
 }
--- src/tools/position.java
+++ src/tools/position.java
@@ -1,17 +1,12 @@
 package tools;
 
-public class position {
+public class Position {
 
 private int posX;
 private int posY;
 
 public void position(int posX, int posY) {
 	this.posX = posX;
-	this.posY = posY;
-}
-
-public void update(int posX, int posY) {
-    this.posX = posX;
 	this.posY = posY;
 }
 
