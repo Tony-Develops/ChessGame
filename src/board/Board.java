6fac4a26a476ed765a403d80dc048a4c67564e60
Tony <s3721573@student.rmit.edu.au>
2018-08-17 12:18:25 +1000

    Added various features into Board class
    Added in move, place and getType.
    Move will move the chess piece to the destination. However, when theres a piece thats already there and is the opposing piece, it will overwrite that piece and return that opposing piece.

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,5 +1,20 @@
 package board;
 
-public class Board {
+import chessPieces.ChessPiece;
 
+public class Board {
+	
+	ChessPiece[][] board;
+	
+	public Board(int sizeX, int sizeY)
+	{
+		if(sizeX < 6 || sizeY < 4)
+		{
+			System.out.println("Size must be of dimension 6x4 or greater!");
+		}
+		else
+		{
+			board = new ChessPiece[sizeX][sizeY];
+		}
+	}
 }
