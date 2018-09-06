b1fd10d33a66cb0015cbae21083cb390a5c323d2
Daniel Dao <guangdanny@gmail.com>
2018-09-07 00:15:14 +1000

    Change board contructor and implement board display

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,6 +1,7 @@
 package board;
 
 import org.junit.Test;
+import java.util.Scanner;
 
 import chessPieces.ChessPiece;
 import exceptions.*;
@@ -17,11 +18,14 @@ public class Board {
 	final int SIZE_Y_FLOOR = 4;
 	final int SIZE_X_CEILING = 10;
 	final int SIZE_Y_CEILING = 8;
-	
-	public Board(int sizeX, int sizeY) throws BoardException
-	{
-		if(sizeX < SIZE_X_FLOOR || sizeY < SIZE_Y_FLOOR)
-		{
+
+	public Board() throws BoardException {
+		Scanner in = new Scanner(System.in);
+		System.out.printf("Please enter the width of the board:  ");
+		int sizeX = in.nextInt();
+		System.out.printf("Please enter the height of the board:  ");
+		int sizeY = in.nextInt();
+		if (sizeX < SIZE_X_FLOOR || sizeY < SIZE_Y_FLOOR) {
 			throw new BoardException("Size must be of dimension 6x4 or greater!");
 		} else if (sizeX > SIZE_X_CEILING || SIZE_Y_CEILING > 8) {
 			throw new BoardException("Board size must not be greater than a dimension of 10x8!");
@@ -33,22 +37,20 @@ public class Board {
 	}
 
 	public void displayBoard() {
-		for (int i = 0; i < this.sizeX; i++) {
-			for (int j = 0; j < this.sizeY; j++) {
-				System.out.println("[  ]");
-			}
+		for (int i = 0; i < sizeX; i++) {
 			System.out.println("");
+			for (int j = 0; j < sizeY; j++) {
+				System.out.print("[ ]");
+			}
 		}
+		System.out.println("");
 	}
 
-	
-	public void place(ChessPiece chessPiece) throws PlaceException
-	{
+	public void place(ChessPiece chessPiece) throws PlaceException {
 		int posX = chessPiece.getPos().getPosX();
 		int posY = chessPiece.getPos().getPosY();
-		
-		if(posX > sizeX - 1 || posY > sizeY - 1)
-		{
+
+		if (posX > sizeX - 1 || posY > sizeY - 1) {
 			throw new PlaceException("Unable to place chesspiece there");
 		}
 		board[posX][posY] = chessPiece;
@@ -58,10 +60,8 @@ public class Board {
 		ChessPiece chessPiece = board[pos.getPosX()][pos.getPosY()];
 		ChessPiece destChessPiece = board[destPos.getPosX()][destPos.getPosY()];
 
-		
-		//Checks to see if the selected chess piece exist
-		if(chessPiece == null)
-		{
+		// Checks to see if the selected chess piece exist
+		if (chessPiece == null) {
 			throw new MovementException("Chess piece doesnt exist");
 
 		}
@@ -77,12 +77,10 @@ public class Board {
 			throw new MovementException("Destination for that piece is invalid");
 		}
 
-		
-		//If there is a chess piece in the destination and checks if its the same colour
-		if(destChessPiece != null)
-		{
-			if(destChessPiece.getType() == type)
-			{
+		// If there is a chess piece in the destination and checks if its the same
+		// colour
+		if (destChessPiece != null) {
+			if (destChessPiece.getType() == type) {
 				throw new MovementException("Cannot move into your own chess piece!");
 			}
 		}
@@ -92,15 +90,12 @@ public class Board {
 		board[pos.getPosX()][pos.getPosY()] = null;
 		return destChessPiece;
 	}
-	
-	private boolean checkMovement(ChessPiece chessPiece, Position destPos)
-	{
-		//Checks for the possible positions for the chess piece
+
+	private boolean checkMovement(ChessPiece chessPiece, Position destPos) {
+		// Checks for the possible positions for the chess piece
 		Position[] possiblePos = chessPiece.calcPositions(board);
-		for(int i = 0; i < possiblePos.length; i++)
-		{
-			if(destPos.getPosX() == possiblePos[i].getPosX() && destPos.getPosY() == possiblePos[i].getPosY())
-			{
+		for (int i = 0; i < possiblePos.length; i++) {
+			if (destPos.getPosX() == possiblePos[i].getPosX() && destPos.getPosY() == possiblePos[i].getPosY()) {
 				return true;
 			}
 		}
--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,14 +1,20 @@
 package main;
 
 import login.Login;
+import board.Board;
+import exceptions.BoardException;
 
 public class Driver {
 
 	
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) throws BoardException 
+	{	
+		
 		Login login = new Login();
 		login.run();
+		
+		Board board = new Board();
+		board.displayBoard();
 
 	}
 
