589160e1f841202f389a45eab8d6c88f5bb8551f
Raaid <s3719919@student.rmit.edu.au>
2018-09-06 21:19:01 +1000

    Finished login
    login finished + test cases 
    more test cases can be added but i've done what was on the suggested milestone 1 spec

--- src/login/LoginTest.java
+++ src/login/LoginTest.java
@@ -2,18 +2,48 @@ package login;
 
 import static org.junit.Assert.*;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
+
+import exceptions.ChessPieceException;
+import exceptions.LoginException;
 
 public class LoginTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	private Login login;
+	
+	@Before
+	public void setUp() throws LoginException 
+	{
+		login = new Login();
 	}
 
+	@Test 
+	public void checkCorrectLoginForRaaid() throws LoginException 
+	{
+		String check = login.getDetails("TEMP");
+		assertEquals(check,"Raaid");
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void checkCorrectLoginForTony() throws LoginException 
+	{
+		String check = login.getDetails("TEMP");
+		assertEquals(check,"Tony");
 	}
 
+	@Test (expected = LoginException.class)
+	public void checkIncorrectPasswordForRaaid() throws LoginException
+	{
+		String check = login.getDetails("TEMP");
+	}
+	
+	@Test (expected = LoginException.class)
+	public void checkClashingUserName() throws LoginException
+	{
+		System.out.println("Player Two Login");
+		login.getDetails("Raaid");
+	}
+	
+	
 }
