4694c7f67db3770505cc75b34a7dcb47ea465e1a
Daniel <s3719271@student.rmit.edu.au>
2018-09-07 01:47:47 +1000

    Bishop fix

--- src/chessPieces/Bishop.java
+++ src/chessPieces/Bishop.java
@@ -15,28 +15,28 @@ public class Bishop extends ChessPiece {
 	public Position[] calcPositions(ChessPiece[][] board) {
 		List<Position> positions = new ArrayList<Position>();
 		Position currentPosition;
-		for (int i = 1; (pos.getPosX() + i) < board.length && (pos.getPosY() + i) < board[0].length; i ++) {
+		for (int i = 1; (pos.getPosX() + i) < board.length && (pos.getPosY() + i) < board[0].length && i <= 2; i ++) {
 			currentPosition = new Position(pos.getPosX() + i, pos.getPosY() + i);
 			positions.add(currentPosition);
 			if (checkPosition(board, currentPosition) == true) {
 				break;
 			}
 		}
-		for (int i = 1; (pos.getPosX() + i) < board.length && (pos.getPosY() - i) > 0; i ++) {
+		for (int i = 1; (pos.getPosX() + i) < board.length && (pos.getPosY() - i) > 0 && i <= 2; i ++) {
 			currentPosition = new Position(pos.getPosX() + i, pos.getPosY() - i);
 			positions.add(currentPosition);
 			if (checkPosition(board, currentPosition) == true) {
 				break;
 			}
 		}
-		for (int i = 1; (pos.getPosX() - i) > 0 && (pos.getPosY() + i) < board[0].length; i ++) {
+		for (int i = 1; (pos.getPosX() - i) > 0 && (pos.getPosY() + i) < board[0].length && i <= 2; i ++) {
 			currentPosition = new Position(pos.getPosX() - i, pos.getPosY() + 1);
 			positions.add(currentPosition);
 			if (checkPosition(board, currentPosition) == true) {
 				break;
 			}
 		}
-		for (int i = 1; (pos.getPosX() - i) > 0 && (pos.getPosY() - i) > 0; i ++) {
+		for (int i = 1; (pos.getPosX() - i) > 0 && (pos.getPosY() - i) > 0 && i <= 2; i ++) {
 			currentPosition = new Position(pos.getPosX() - i, pos.getPosY() - 1);
 			positions.add(currentPosition);
 			if (checkPosition(board, currentPosition) == true) {
